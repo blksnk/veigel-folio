7a2735a48166ba893ec45b3a553acc24447b425c
JN Veigel <art.jnveigel@gmail.com>
2019-11-25 19:11:35 +0100

    :speech_balloon: wording modifications :recycle: refactor emphasis component

--- src/views/About.js
+++ src/views/About.js
@@ -4,12 +4,16 @@ import { alignAbout } from 'helpers/alignLayout.js'
 import { FullSection } from 'components/Section.js'
 import bg3 from 'assets/images/bg3.jpg'
 
-const About = ({ underlineLinks }) => {
+const About = () => {
+  const [ loaded, setLoaded ] = React.useState(false)
   React.useEffect(() => {
-    alignAbout()
-  })
+    if(!loaded) {
+      setTimeout(() => alignAbout(), 100)
+      setLoaded(true)
+    }
+  }, [ loaded ])
   return (
-    <FullSection className={s.page} id='section2' onEnter={() => underlineLinks(2)}>
+    <FullSection className={s.page} id='section2'>
       <div className={s.left}></div>
       <div className={s.image} style={{ backgroundImage: `url(${bg3})` }}></div>
 
@@ -19,19 +23,20 @@ const About = ({ underlineLinks }) => {
             Hi, I'm Jean-Nicolas
           </h1>
           <p>
-            I'm a freelance full stack developer based in <Underline>Strasbourg, France</Underline>.
-            I like to focus on <Underline>modern</Underline> layouts, <Underline>creative</Underline> animations and <Underline>clean code</Underline> all around.
+            I'm a freelance full stack developer based in <Emphasis>Strasbourg, France</Emphasis>.
+            I like to focus on <Emphasis>modern</Emphasis> layouts, <Emphasis>creative</Emphasis> animations and <Emphasis>clean code</Emphasis> all around.
           </p>
 
           <p>
             I have worked with individuals and at start-ups, agencies and brands.
           <br/>
-            Over the past year, I've teamed up with <Underline linkTo='wolfox.co'>Wolfox</Underline>, <Underline>DataValue Consulting</Underline>, <Underline>Schneider Electric</Underline> and more.
+            Over the past year, I've teamed up with <Emphasis linkTo='wolfox.co'>Wolfox</Emphasis>, <Emphasis linkTo='datavalue-consulting.com'>DataValue Consulting</Emphasis>, <Emphasis linkTo='se.com'>Schneider Electric</Emphasis> and more.
           </p>
 
           <p>
-            I speak a couple of languages fluently, namely: French, English and German.
-            <br/>
+            I speak a couple of languages fluently, namely <Emphasis>French</Emphasis>, <Emphasis>English</Emphasis> and <Emphasis>German</Emphasis>.
+          </p>
+          <p>
             Communication should be a non-issue.
           </p>
         </div>
@@ -40,9 +45,9 @@ const About = ({ underlineLinks }) => {
   )
 }
 
-const Underline = ({ children, linkTo }) => {
+const Emphasis = ({ children, linkTo }) => {
   if(!linkTo) {
-    return <span className={s.underline}>{children}</span>
+    return <span className={s.white}>{children}</span>
   } else {
     return <a className={`${s.underline} link`} href={`https://www.${linkTo}`} target='_blank' title={`Visit ${String(children)}'s website`} rel='noopener noreferrer'>{children}</a>
   }
