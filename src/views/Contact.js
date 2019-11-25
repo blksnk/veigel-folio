e77773101bcac17e06417e135b09158f0cdecac2
JN Veigel <art.jnveigel@gmail.com>
2019-11-25 19:10:38 +0100

    :fire: remove credit component, nowhere to put it in ui :speech_balloon: change wording

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { HalfSection, Link } from 'components/Section.js'
 import s from './Contact.module.css'
 
-const Contact = ({ underlineLinks }) => {
+const Contact = () => {
   return (
-    <HalfSection onEnter={() => underlineLinks(3)} onLeave={() => underlineLinks(2)} className={s.page} left  id='section3'>
+    <HalfSection className={s.page} left  id='section3'>
       <p className={s.text}>
-        For professional opportunities, general information or just to send me jokes, please get in touch at the email below.
+        If you wish to keep up to date on my work, feel free to follow me on my socials.
+      </p>
+      <p className={s.text}>
+        For professional opportunities, general information or to send memes, <br/> please get in touch at the email below.
       </p>
       <Link className={s.email} img title='send me an email' href='mailto:hello@veigel.dev'>hello@veigel.dev</Link>
       {/*<Credit/>*/}
@@ -14,11 +17,4 @@ const Contact = ({ underlineLinks }) => {
   )
 }
 
-const Credit = () => (
-  <span className={s.credit}>
-    Photos by <Link className={s.creditLink} title="Visit Paweł's profile on Unsplash" href="https://unsplash.com/@pawel_czerwinski">
-      Paweł Czerwiński
-    </Link>
-  </span>
-)
 export default Contact
